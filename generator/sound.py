c75212cfe0ed76093b31fc1a070d14cb027ebfc1
Aluriak <lucas.bourneuf@laposte.net>
2016-11-26 23:07:09 +0100

    fixup! gen: sound: wave file player

--- generator/sound.py
+++ generator/sound.py
@@ -1,5 +1,8 @@
 """Cross platform sound management,
 heavily inspired from http://stackoverflow.com/a/311634.
+The unix part is not enough: writing in /dev/dsp is blocking,
+and implementation using multiple threads lead to threads blocking each other,
+so not all notes are played.
 
 Could require to load the snd-pcm-oss kernel module:
     sudo modprobe snd-pcm-oss
@@ -29,18 +32,10 @@ WAV_FILE_TEMPLATE = "./Matlab/PianoNote/{}.wav"
 def play_midi(code:int):
     wavefile = WAV_FILE_TEMPLATE.format(code)
     if UNIX:
-        t = threading.Thread(target=_unix_plays, args=[wavefile])
-        t.daemon = True
-        t.start()
+        with open_wave(wavefile, 'rb') as fd, open_oss('/dev/dsp', 'w') as dsp:
+            nc, sw, fr, nf, comptype, compname = fd.getparams()
+            dsp.setparameters(AFMT_S16_NE, nc, fr)
+            data = fd.readframes(nf)
+            dsp.write(data)
     else:
         winsound.PlaySound(wavefile, winsound.SND_FILENAME)
-
-
-def _unix_plays(wavefile:str):
-    assert UNIX
-    with open_wave(wavefile, 'rb') as fd, open_oss('/dev/dsp', 'w') as dsp:
-        nc, sw, fr, nf, comptype, compname = fd.getparams()
-        dsp.setparameters(AFMT_S16_NE, nc, fr)
-        data = fd.readframes(nf)
-        dsp.write(data)
-    print('TOGO')
