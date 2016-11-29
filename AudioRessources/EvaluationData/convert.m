beda6c61fe56a08ef5377d3cb0e229594874b362
Dupont <samuel.dupont.etu@univ-lemans.fr>
2016-11-29 12:09:26 +0100

    Prog to do eval set

--- AudioRessources/EvaluationData/convert.m
+++ AudioRessources/EvaluationData/convert.m
@@ -1,14 +1,34 @@
+clear all; close all;clc
 
-[data, fs]=audioread('chpn_op23.wav');
+%% read midi
+  fs=44100;
+  filename='chpn_op23';
+
+  midi=readmidi(['../' filename '.mid']);
+  Notes = midiInfo(midi,0);
+  out=[Notes(:,5)*fs ,Notes(:,3)];
+  
+
+%% read wav
+ [data, fs]=audioread(['../' filename '.wav'] );
+% [data,fs] = midi2audio(midi);    
 taille = length(data);
 
-T=10*fs;
+T=5*fs;
 NSPLIT=floor(taille/T);
+
+
 %%
-for ii=1:2
-    filename=['test' num2str(ii)]
-[ debut ] = closest( (ii-1)*T,out(:,1) )
-[ fin ] = closest( (ii)*T,out(:,1) )
+% plot(data(1:1.581237e+06))
+% hold on
+% stem(out(1:102,1),ones(102,1))
+
+%% split
+cont=1;
+
+for ii=1:NSPLIT
+[ debut ] = closest( (ii-1)*T+1,out(:,1) );
+[ fin ] = closest( (ii)*T,out(:,1) );
 
 while out(debut,1) < (ii-1)*T
    debut=debut+1; 
@@ -18,13 +38,22 @@ while out(fin,1) > (ii)*T
    fin=fin-1; 
 end
 
- audiowrite([filename '.wav'],data(((ii-1)*T+1):(ii*T)),fs);
  pos=(out(debut:fin,1)-((ii-1)*T))/fs;
- dlmwrite([filename '.txt'],pos,'delimiter','\t','precision',9);
- 
- plot(abs(data(((ii-1)*T+1):(ii*T))))
- hold on
-stem(pos*fs,ones(length(pos),1))
-k = waitforbuttonpress;
-close
+ num(ii)=length(out(debut:fin,1));
+     if length(out(debut:fin,1))<=18
+     figure
+     plot(data((ii-1)*T+1:(ii)*T))
+     hold on
+     stem(pos*fs,ones(length(pos),1)-0.5)
+     sound(data((ii-1)*T+1:(ii)*T),fs)
+     filename=['TestlowDens' num2str(cont)]
+%        audiowrite([filename '.wav'],data(((ii-1)*T+1):(ii*T)),fs);
+%       dlmwrite([filename '.txt'],pos,'delimiter','\t','precision',9);
+      k = waitforbuttonpress;
+
+
+     cont=cont+1;
+    close
+     end
 end
+
